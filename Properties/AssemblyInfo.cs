b94ea346b19c1943ae84312e1fbc84be89b19757
leemellaret <leemellaret@gmail.com>
2020-03-10 21:59:10 +0500

    Немного исправил свойства проекта

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанные с этой сборкой.
-[assembly: AssemblyTitle("Evolution-Simulator")]
+[assembly: AssemblyTitle("EvolutionSimulator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Evolution-Simulator")]
+[assembly: AssemblyProduct("EvolutionSimulator")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
